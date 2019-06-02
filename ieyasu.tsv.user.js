eb5d0f3045b9dff305286d13c06e9574864927fa
Diep Vo <VoNgocDiep102@gmail.com>
2019-06-02 20:31:31 +0900

    formadddddddddddd

--- ieyasu.tsv.user.js
+++ ieyasu.tsv.user.js
@@ -8,30 +8,30 @@
     const getTextWrap = element => '"' + getText(element) + '"';
 
     const getCellData = (cell, index, timePrefix) => {
-            if (index) {
-                if (index > 1 && index < 7) {
-                    const matches = getText(cell).match(/\d+:\d+/);
-                    if (matches && matches.length) {
-                        return matches[0];
-                    }
+        if (index) {
+            if (index > 1 && index < 7) {
+                const matches = getText(cell).match(/\d+:\d+/);
+                if (matches && matches.length) {
+                    return matches[0];
                 }
-                return getTextWrap(cell);
-            } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
-        }
-        (() => {
-            const timePrefix = byId("select").value.replace("-", "/");
-            const tsv = mapCall(
-                byId("editGraphTable")[chils][0][chils],
-                (row, i) => mapCall(
-                    row[chils],
-                    (cell, j) => i == 0 ? getTextWrap(cell) : getCellData(cell, j, timePrefix)
-                ).join("\t")
-            ).join("\n");
-            const a = documentx.createElement("a");
-            a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
-            a.setAttribute("download", "data.tsv");
-            documentx.body.appendChild(a);
-            a.click();
-            documentx.body.removeChild(a);
-        })();
+            }
+            return getTextWrap(cell);
+        } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
+    }
+    (() => {
+        const timePrefix = byId("select").value.replace("-", "/");
+        const tsv = mapCall(
+            byId("editGraphTable")[chils][0][chils],
+            (row, i) => mapCall(
+                row[chils],
+                (cell, j) => i == 0 ? getTextWrap(cell) : getCellData(cell, j, timePrefix)
+            ).join("\t")
+        ).join("\n");
+        const a = documentx.createElement("a");
+        a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
+        a.setAttribute("download", "data.tsv");
+        documentx.body.appendChild(a);
+        a.click();
+        documentx.body.removeChild(a);
+    })();
 })();
