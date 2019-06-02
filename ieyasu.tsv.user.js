1777ac2ae9cbf574db63984434ac702ac44bd768
Diep Vo <VoNgocDiep102@gmail.com>
2019-06-02 17:27:16 +0900

    optimize for minimize

--- ieyasu.tsv.user.js
+++ ieyasu.tsv.user.js
@@ -1,72 +1,34 @@
 (function() {
-    'use strict';
-    
-    const COL_DAY = 0;
-    const COL_WORK = 1;
-    const COL_START = 2;
-    const COL_END = 3;
-    const COL_TOTAL = 4;
-    const COL_BREAK = 5;
-    const COL_OVER = 6;
-    const COL_COMMENT = 7;
-    const COL_CONFIRM = 8;
 
+    documentx = document
+    mapCall = (col, fun) => [].map.call(col, fun);
+    byId = id => documentx.getElementById(id);
+    chills = "children";
+    getText = element => element.innerText.trim();
+    getTextWrap = element => '"'+getText(element)+'"';
 
-    function getCellData(cells, index, meta) {
-        let text = cells[index].innerText.trim();
-        switch (index) {
-            case COL_DAY:
-                return meta.year + "/" + meta.month + "/" + text.match(/\d+/)[0];
-            case COL_START:
-            case COL_END:
-            case COL_TOTAL:
-            case COL_BREAK:
-            case COL_OVER:
-                let matches = text.match(/\d+:\d+/);
+    function getCellData(cell, index, timePrefix) {
+        if (index) {
+            if (index > 1 && index < 7) {
+                matches = getText(cell).match(/\d+:\d+/);
                 if (matches && matches.length) {
                     return matches[0];
-                } else {
-                    return text;
-                }
-            default:
-                return '"' + text + '"';
-        }
-    }
-
-    function getColHead(cells, index) {
-        return "\"" + cells[index].innerText.trim() + "\"";
-    }
-
-    window.exportTsv = function() {
-        let table = document.getElementById('editGraphTable');
-        let rows = table.firstElementChild.children;
-        let rawData = [];
-        for (let i = 0; i < rows.length; i += 1) {
-            let cells = rows[i].children;
-            for (let j = 0; j < cells.length; j += 1) {
-                if (j != 0) {
-                    rawData.push("\t");
-                }
-                if (i == 0) {
-                    rawData.push(getColHead(cells, j));
-                } else {
-                    rawData.push(getCellData(cells, j, {
-                        month: 3,
-                        year: 2019
-                    }));
                 }
             }
-            rawData.push("\n");
-        }
-        console.log(rawData.join(""));
-        let a = document.createElement("a");
-        a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(rawData.join(""));
-        a.setAttribute("download", "data.tsv");
-
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
+            return getTextWrap(cell);
+        } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
     }
-    window.exportTsv();
-
+    timePrefix = byId("select").value.replace("-", "/");
+    rows = mapCall(
+        byId("editGraphTable")[chills][0][chills],
+        (row, i) => mapCall(
+            row[chills],
+            (cell, j) => i == 0 ? getTextWrap(cell): getCellData(cell, j, timePrefix)
+        ).join("\t")
+    ).join("\n");
+    a = documentx.createElement("a");
+    a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(rawData.join(""));
+    a.setAttribute("download", "data.tsv");
+    documentx.body.appendChild(a);
+    a.click();
 })();
