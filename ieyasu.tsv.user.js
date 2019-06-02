ae1da751897e990a5a569d5bc87ecc14751f5149
Diep Vo <VoNgocDiep102@gmail.com>
2019-06-02 20:19:37 +0900

    format

--- ieyasu.tsv.user.js
+++ ieyasu.tsv.user.js
@@ -7,7 +7,6 @@
 // @match        https://p.ieyasu.co/works*
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
 
@@ -17,7 +16,7 @@
         parent.appendChild(button);
         button.outerHTML = '<div class="floatR" style="margin-right:10px;"><a class="btn" onclick="window.exportTsv()">TSV出力</a></div>';
     }
-    
+
     init();
 
     const documentx = document;
@@ -28,30 +27,30 @@
     const getTextWrap = element => '"' + getText(element) + '"';
 
     const getCellData = (cell, index, timePrefix) => {
-            if (index) {
-                if (index > 1 && index < 7) {
-                    const matches = getText(cell).match(/\d+:\d+/);
-                    if (matches && matches.length) {
-                        return matches[0];
-                    }
+        if (index) {
+            if (index > 1 && index < 7) {
+                const matches = getText(cell).match(/\d+:\d+/);
+                if (matches && matches.length) {
+                    return matches[0];
                 }
-                return getTextWrap(cell);
-            } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
-        }
-        window.exportTsv = () => {
-            const timePrefix = byId("select").value.replace("-", "/");
-            const tsv = mapCall(
-                byId("editGraphTable")[chils][0][chils],
-                (row, i) => mapCall(
-                    row[chils],
-                    (cell, j) => i == 0 ? getTextWrap(cell) : getCellData(cell, j, timePrefix)
-                ).join("\t")
-            ).join("\n");
-            const a = documentx.createElement("a");
-            a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
-            a.setAttribute("download", "data.tsv");
-            documentx.body.appendChild(a);
-            a.click();
-            documentx.body.removeChild(a);
-        };
+            }
+            return getTextWrap(cell);
+        } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
+    }
+    window.exportTsv = () => {
+        const timePrefix = byId("select").value.replace("-", "/");
+        const tsv = mapCall(
+            byId("editGraphTable")[chils][0][chils],
+            (row, i) => mapCall(
+                row[chils],
+                (cell, j) => i == 0 ? getTextWrap(cell) : getCellData(cell, j, timePrefix)
+            ).join("\t")
+        ).join("\n");
+        const a = documentx.createElement("a");
+        a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
+        a.setAttribute("download", "data.tsv");
+        documentx.body.appendChild(a);
+        a.click();
+        documentx.body.removeChild(a);
+    };
 })();
