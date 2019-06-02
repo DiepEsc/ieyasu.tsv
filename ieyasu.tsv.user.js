a14dc95ce0cc9ecd7c4c9ebc382cd99befe431f6
Diep Vo <VoNgocDiep102@gmail.com>
2019-06-02 18:26:33 +0900

    forrmat

--- ieyasu.tsv.user.js
+++ ieyasu.tsv.user.js
@@ -1,37 +1,37 @@
 (() => {
 
-    const documentx = document
+    const documentx = document;
     const mapCall = (col, fun) => [].map.call(col, fun);
     const byId = id => documentx.getElementById(id);
-    const chills = "children";
+    const chils = "children";
     const getText = element => element.innerText.trim();
-    const getTextWrap = element => '"'+getText(element)+'"';
+    const getTextWrap = element => '"' + getText(element) + '"';
 
     const getCellData = (cell, index, timePrefix) => {
-        if (index) {
-            if (index > 1 && index < 7) {
-                const matches = getText(cell).match(/\d+:\d+/);
-                if (matches && matches.length) {
-                    return matches[0];
+            if (index) {
+                if (index > 1 && index < 7) {
+                    const matches = getText(cell).match(/\d+:\d+/);
+                    if (matches && matches.length) {
+                        return matches[0];
+                    }
                 }
-            }
-            return getTextWrap(cell);
-        } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
-    }
-    (() => {
-        const timePrefix = byId("select").value.replace("-", "/");
-        const tsv = mapCall(
-            byId("editGraphTable")[chills][0][chills],
-            (row, i) => mapCall(
-                row[chills],
-                (cell, j) => i == 0 ? getTextWrap(cell): getCellData(cell, j, timePrefix)
-            ).join("\t")
-        ).join("\n");
-        const a = documentx.createElement("a");
-        a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
-        a.setAttribute("download", "data.tsv");
-        documentx.body.appendChild(a);
-        a.click();
-        documentx.body.removeChild(a);
-    })();
+                return getTextWrap(cell);
+            } else return timePrefix + "/" + getText(cell).match(/\d+/)[0];
+        }
+        (() => {
+            const timePrefix = byId("select").value.replace("-", "/");
+            const tsv = mapCall(
+                byId("editGraphTable")[chils][0][chils],
+                (row, i) => mapCall(
+                    row[chils],
+                    (cell, j) => i == 0 ? getTextWrap(cell) : getCellData(cell, j, timePrefix)
+                ).join("\t")
+            ).join("\n");
+            const a = documentx.createElement("a");
+            a.href = "data:text/plain;charset=utf-8," + encodeURIComponent(tsv);
+            a.setAttribute("download", "data.tsv");
+            documentx.body.appendChild(a);
+            a.click();
+            documentx.body.removeChild(a);
+        })();
 })();
